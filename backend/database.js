63dacc4553c144d1da9e8a1b4ae65214e7b034fd
lianaprtw <liana.pratiwi15@gmail.com>
2025-09-22 17:30:11 +0800

    Membuat database.js dan index.js serta mengedit package.json di folder backend

---
+++ backend/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const databaseConnection = async () => {
+    mongoose.connect("mongodb://localhost:27017/book")
+    .then(() => {
+        console.log("Database connected successfully!");
+    })
+    .catch((err) => {
+        console.log("Database connection failed!", err);
+    });
+};
+
+module.exports = databaseConnection;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("Hello from backend");
+const express = require("express");
+const databaseConnection = require("./database")
+
+// database connection
+databaseConnection();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Backend!");
+});
+
+app.listen(8000, () => {
+    console.log("Port listening on port 8000");
+});
