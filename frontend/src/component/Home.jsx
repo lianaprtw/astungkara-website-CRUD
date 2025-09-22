6f95b1b9817f17112fda260244c5fe955bc9d212
lianaprtw <liana.pratiwi15@gmail.com>
2025-09-22 22:59:46 +0800

    Menginstall axios, membuat file axios, mengedit file index.js di folder backend dan file home.jsx di folder frontend

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { guestBaseUrl } from "../axiosInstance";
 
 const Home = () => {
+  const [guestForm, setGuestForm] = useState({
+    GuestName: "",
+    Address: "",
+    Phone: "",
+    Message: "",
+    PublishDate: "",
+  });
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setGuestForm((prev) => ({
+      ...prev,
+      [name]: value, 
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const data = await guestBaseUrl.post("/addguest", guestForm);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log('guestForm', guestForm);
+
   return (
     <div className="w-full px-5 min-h-[calc(100vh-60px)]">
       <div className="w-full grid grid-cols-5 gap-3 my-4">
@@ -10,6 +38,9 @@ const Home = () => {
             type="text"
             placeholder="Name"
             className="w-full border-2  text-gray-800 border-gray-300 rounded-sm outline-none h-8 px-2"
+            name="GuestName"
+            value={guestForm.GuestName}
+            onChange={handleFormChange}
           />
         </div>
         <div className="w-full flex flex-col gap-2">
@@ -18,6 +49,9 @@ const Home = () => {
             type="text"
             placeholder="Address"
             className="w-full border-2 text-gray-800 border-gray-300 rounded-sm outline-none h-8 px-2"
+            name="Address"
+            value={guestForm.Address}
+            onChange={handleFormChange}
           />
         </div>
         <div className="w-full flex flex-col gap-2">
@@ -26,6 +60,9 @@ const Home = () => {
             type="text"
             placeholder="Phone"
             className="w-full border-2 text-gray-800 border-gray-300 rounded-sm outline-none h-8 px-2"
+            name="Phone"
+            value={guestForm.Phone}
+            onChange={handleFormChange}
           />
         </div>
         <div className="w-full flex flex-col gap-2">
@@ -34,6 +71,9 @@ const Home = () => {
             type="text"
             placeholder="Message "
             className="w-full border-2 text-gray-800 border-gray-300 rounded-sm outline-none h-8 px-2"
+            name="Message"
+            value={guestForm.Message}
+            onChange={handleFormChange}
           />
         </div>
         <div className="w-full flex flex-col gap-2">
@@ -42,11 +82,14 @@ const Home = () => {
             type="date"
             placeholder="Publish Date"
             className="w-full border-2 text-gray-800 border-gray-300 rounded-sm outline-none h-8 px-2"
+            name="PublishDate"
+            value={guestForm.PublishDate}
+            onChange={handleFormChange}
           />
         </div>
       </div>
       <div className="w-full flex justify-end">
-        <button className="bg-gray-700 text-white h-9 w-22 rounded-md cursor-pointer">
+        <button className="bg-gray-700 text-white h-9 w-22 rounded-md cursor-pointer" onClick={handleSubmit}>
           SUBMIT
         </button>
       </div>
@@ -74,7 +117,6 @@ const Home = () => {
                 <th className="tracking-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
                   Action
                 </th>
-
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
